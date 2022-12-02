6f30c7b6b68d6a105f0f5cd2e130a3fc8d6ab85e
Zoe Hay <zoemhay@gmail.com>
2022-12-02 13:27:10 -0800

    change users to user, user id instead of req param

---
+++ tests/userAuth.test.js
@@ -0,0 +1,66 @@
+const request = require("supertest");
+const app = require("../src/app");
+const { prisma } = require("../repository/repository");
+const { exec } = require("child_process");
+
+let cookie;
+let userId;
+let userEmail = "john@goods.com";
+let userPassword = "stuff";
+let nonAuthId;
+let nonAuthEmail = "fletch@email.com";
+
+afterAll(async () => {
+  await prisma.$disconnect();
+});
+
+beforeAll(async () => {
+  // clear and seed the Product and User tables
+  // await prisma.$executeRawUnsafe(
+  //   `ALTER SEQUENCE "Product_id_seq" RESTART WITH 1`
+  // );
+  // await prisma.$executeRawUnsafe(`TRUNCATE TABLE "Product" CASCADE;`);
+  // await prisma.$executeRawUnsafe(`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`);
+  // await prisma.$executeRawUnsafe(`TRUNCATE TABLE "User" CASCADE;`);
+  // await prisma.$executeRawUnsafe(`TRUNCATE TABLE "Session" CASCADE;`);
+  // await exec("node ../prisma/seed.js");
+});
+
+beforeEach(async () => {
+  // user Id of user to authorize
+  const user = await prisma.user.findUnique({
+    where: { email: userEmail },
+  });
+  userId = user.id;
+  // user Id of a different user
+  const nonAuthUser = await prisma.user.findUnique({
+    where: { email: nonAuthEmail },
+  });
+  nonAuthId = nonAuthUser.id;
+  // log user in before each test
+  const userLogin = {
+    email: userEmail,
+    password: userPassword,
+  };
+
+  const response = await request(app).post("/auth/login").send(userLogin);
+  console.log(response.headers["set-cookie"]);
+  cookie = response.headers["set-cookie"];
+});
+
+afterEach(async () => {
+  // log the user out after each test
+  const response = await request(app)
+    .post("/auth/logout")
+    .set("Cookie", cookie);
+});
+
+test("Logged in user gets all of their account information", async () => {
+  const response = await request(app).get(`/user`).set("Cookie", cookie);
+  console.log(response.body);
+  expect(response.body.user.email).toEqual(userEmail);
+});
+
+// test("Logged in user updates their name");
+
+// test("Logged in user updates their password");
