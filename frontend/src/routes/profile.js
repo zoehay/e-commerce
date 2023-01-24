cc39a289c4bf7afae97eed8c4d3ec5db43094a21
Zoe Hay <zoemhay@gmail.com>
2023-01-23 17:38:37 -0800

    start profile page, maintain user context

--- frontend/src/routes/profile.js
+++ frontend/src/routes/profile.js
@@ -1,23 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import ProductFeed from "../components/ProductFeed";
 import ProductTile from "../components/ProductTile";
 import Client from "../util/Client";
+import { UserContext, UserProvider } from "../util/userContext";
 
 const Profile = () => {
-  let [userDetails, setUserDetails] = useState({});
+  // let [userDetails, setUserDetails] = useState({});
 
-  useEffect(() => {
-    async function fetchData() {
-      let user = await Client.getUser();
-      setUserDetails(user);
-      console.log("profile use effect", user);
-    }
-    fetchData();
-  }, []);
+  const context = useContext(UserContext);
+  let details = context.user;
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     let userDetails = await Client.getUser();
+  //     setUserDetails(userDetails);
+  //   }
+  //   fetchData();
+  // }, []);
 
   return (
     <div>
-      <h1>details{userDetails.email}</h1>
+      <h1>Account Details</h1>
+      <h2>{details.email}</h2>
+      <h2>{details.name}</h2>
+      <h2>a button to edit</h2>
     </div>
   );
 };
