3621b947f486d5d712dd177d0ed07cca6d775c2e
Zoe Hay <zoemhay@gmail.com>
2023-01-26 11:25:59 -0800

    updated seed script devseed

---
+++ backend/prisma/devseed.js
@@ -0,0 +1,121 @@
+const { PrismaClient, Prisma } = require("@prisma/client");
+const request = require("supertest");
+const app = require("../src/app");
+const bcrypt = require("bcrypt");
+
+const prisma = new PrismaClient({ log: ["query"] });
+
+async function main() {
+  console.log("Seed");
+
+  await prisma.$executeRawUnsafe(`TRUNCATE TABLE "Session" CASCADE;`);
+  await prisma.$executeRawUnsafe(`TRUNCATE TABLE "CartProduct";`);
+  await prisma.$executeRawUnsafe(`ALTER SEQUENCE "User_id_seq" RESTART WITH 1`);
+  await prisma.$executeRawUnsafe(`TRUNCATE TABLE "User" CASCADE;`);
+  await prisma.$executeRawUnsafe(
+    `ALTER SEQUENCE "Product_id_seq" RESTART WITH 1`
+  );
+  await prisma.$executeRawUnsafe(`TRUNCATE TABLE "Product" CASCADE;`);
+
+  let products = [
+    {
+      name: "Shovel",
+      description: "It's like a big spoon for dirt and stuff.",
+      price: 9.0,
+    },
+    {
+      name: "Bug Net",
+      description: "Catch those creepy crawlies.",
+      price: 11.0,
+    },
+    {
+      name: "Fishing Rod",
+      description: "A rod used for fishing and the like.",
+      price: 12.6,
+    },
+  ];
+
+  await Promise.all(
+    products.map(async (product) => {
+      await prisma.product.create({
+        data: product,
+      });
+    })
+  );
+
+  let users = [
+    {
+      email: "john@email",
+      userName: "John",
+      password: "stuff",
+    },
+    {
+      email: "fletch@email.com",
+      userName: "Fletch",
+      password: "lime",
+    },
+    {
+      email: "ray@email.com",
+      userName: "",
+      password: "plant",
+    },
+  ];
+
+  for (const user of users) {
+    console.log(user);
+    const salt = await bcrypt.genSalt(10);
+    const hashedUserPassword = await bcrypt.hash(user.password, salt);
+    const newUser = await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.userName,
+        password: hashedUserPassword,
+      },
+    });
+    console.log(newUser);
+  }
+
+  let cartProducts = [
+    {
+      userId: 1,
+      productId: 1,
+      quantity: 1,
+    },
+    {
+      userId: 1,
+      productId: 2,
+      quantity: 1,
+    },
+    {
+      userId: 1,
+      productId: 3,
+      quantity: 2,
+    },
+    {
+      userId: 2,
+      productId: 1,
+      quantity: 3,
+    },
+    {
+      userId: 3,
+      productId: 3,
+      quantity: 2,
+    },
+  ];
+
+  await Promise.all(
+    cartProducts.map(async (cartProduct) => {
+      await prisma.cartProduct.create({ data: cartProduct });
+    })
+  );
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
