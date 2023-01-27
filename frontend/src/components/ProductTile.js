106aac36dbc1ca0c5652decc401dc2daefeab586
Zoe Hay <zoemhay@gmail.com>
2023-01-26 16:57:03 -0800

    product tile quantity increment input

--- frontend/src/components/ProductTile.js
+++ frontend/src/components/ProductTile.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import Client from "../util/Client";
 import { UserContext } from "../util/userContext";
@@ -38,16 +38,28 @@ const ProductTile = ({ product }) => {
   const context = useContext(UserContext);
   const user = context.user;
 
+  const [quantity, setQuantity] = useState(1);
+
   return (
     <Tile>
       <ProductInfo>
         <ProductName>{product.name}</ProductName>
         <ProductPrice>{product.price}</ProductPrice>
         <ProductDescription>{product.description}</ProductDescription>
+        <>
+          <label htmlFor="quantityInput">Quantity</label>
+          <input
+            id="quantityInput"
+            name="quantity"
+            type="number"
+            defaultValue={1}
+            onChange={(event) => setQuantity(event.target.value)}
+          />
+        </>
         <AddToCart
           onClick={() => {
-            console.log(user.id, product.id, 1);
-            Client.addCartProduct(user.id, product.id, 1);
+            console.log(user.id, product.id, quantity);
+            Client.addCartProduct(user.id, product.id, quantity);
           }}
         >
           Add to Cart
