d9bf0b2fb36360325f100cf03bc613d89e415ed0
Zoe Hay <zoemhay@gmail.com>
2023-02-06 11:51:22 -0800

    mobile nav bar drop down menu

---
+++ frontend/src/components/MobileNav.js
@@ -0,0 +1,85 @@
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { NavButton, NavSelect } from "./Navbar";
+
+const MobileNavWrapper = styled.div`
+  position: absolute;
+  top: 5rem;
+  display: flex;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+  @media (min-width: 46rem) {
+    max-width: 75rem;
+    justify-content: space-between;
+  }
+`;
+
+const MobileDropdown = styled.div`
+  background-color: var(--accent-light-1);
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  height: 100%;
+  box-shadow: 0.5rem 0.5rem 0.7rem var(--accent-bold-1);
+
+  @media (min-width: 46rem) {
+    width: 33%;
+  }
+`;
+
+const MobileNavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  width: 100%;
+  box-sizing: border-box;
+  list-style-type: none;
+  line-height: 3rem;
+  font-weight: bold;
+`;
+
+const MobileNav = () => {
+  return (
+    <MobileNavWrapper>
+      <MobileDropdown>
+        <MobileNavList>
+          <li>
+            <NavSelect to="/">Home</NavSelect>
+          </li>
+          <li>
+            <NavSelect to="/products">Shop</NavSelect>
+          </li>
+        </MobileNavList>
+      </MobileDropdown>
+    </MobileNavWrapper>
+  );
+};
+
+export const MobileNavExt = () => {
+  let [showMenu, setShowMenu] = useState(false);
+
+  const handleMobileMenu = (showMenu) => {
+    setShowMenu(!showMenu);
+  };
+
+  if (showMenu === false) {
+    return (
+      <NavButton onClick={() => setShowMenu(true)}>
+        <FontAwesomeIcon icon={faBars} />
+      </NavButton>
+    );
+  } else {
+    return (
+      <>
+        <NavButton onClick={() => setShowMenu(false)}>
+          <FontAwesomeIcon icon={faX} />
+        </NavButton>
+        <MobileNav />
+      </>
+    );
+  }
+};
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Navigate, NavLink, useLocation, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { UserContext } from "../util/userContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUser,
   faCartShopping,
   faRightFromBracket,
+  faBars,
+  faX,
 } from "@fortawesome/free-solid-svg-icons";
+import { MobileNavExt } from "./MobileNav";
 
 const Nav = styled.nav`
   height: 5rem;
@@ -24,41 +27,72 @@ const NavBackground = styled.div`
 
 const NavMenu = styled.div`
   display: flex;
-  justify-content: space-between;
   width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
   @media (min-width: 46rem) {
     max-width: 75rem;
+    justify-content: space-between;
   }
 `;
 
 const NavDiv = styled.div`
   display: flex;
   align-items: center;
+  width: min-content;
+
+  box-sizing: border-box;
 `;
 
-const NavSelect = styled(NavLink)`
-  color: #f4f1bb;
-  display: flex;
-  align-items: center;
-  padding: 0 1rem;
-  text-decoration: none;
-  &:hover {
-    color: #ed6a5a;
+const MobileNavDiv = styled(NavDiv)`
+  display: grid;
+  justify-content: left;
+  @media (min-width: 46rem) {
+    display: flex;
+    align-items: center;
   }
 `;
 
-const NavButton = styled.button`
-  margin: 0 1rem;
+export const NavSelect = styled(NavLink)`
+  color: #f4f1bb;
+  display: flex;
+  align-items: center;
+  margin: 0 0.25rem;
+  text-decoration: none;
+  font-size: 1rem;
+  width: 2rem;
+  justify-content: center;
+  box-sizing: border-box;
+  &:hover {
+    color: #ed6a5a;
+  }
+  @media (min-width: 46rem) {
+    margin: 0 1rem;
+  }
+`;
+
+export const NavButton = styled.button`
   color: var(--accent-light-2);
   background-color: transparent;
   border: none;
+  width: 2rem;
+  margin: 0 0.25rem;
   &:hover {
     color: var(--accent-bold-2);
+  }
+  @media (min-width: 46rem) {
+    margin: 0 1rem;
   }
 `;
 
 const NavLogo = styled(NavSelect)`
   font-size: 2rem;
+  padding: 0;
+  width: fit-content;
+  @media (min-width: 46rem) {
+    display: flex;
+    align-items: center;
+  }
 `;
 
 const UserOptions = () => {
@@ -69,7 +103,9 @@ const UserOptions = () => {
   if (!user) {
     return (
       <NavDiv>
-        <NavSelect to="/auth/login">Login</NavSelect>
+        <NavSelect to="/auth/login">
+          <FontAwesomeIcon icon={faUser} />
+        </NavSelect>
         <NavSelect to="/auth/register">Register</NavSelect>
       </NavDiv>
     );
@@ -95,15 +131,14 @@ const UserOptions = () => {
   }
 };
 
-const Navbar = () => {
+export const Navbar = () => {
   return (
     <Nav>
       <NavBackground>
         <NavMenu>
-          <NavDiv>
-            <NavSelect to="/">Home</NavSelect>
-            <NavSelect to="/products">Shop</NavSelect>
-          </NavDiv>
+          <MobileNavDiv>
+            <MobileNavExt></MobileNavExt>
+          </MobileNavDiv>
           <NavLogo to="/">The Store</NavLogo>
           <UserOptions></UserOptions>
         </NavMenu>
